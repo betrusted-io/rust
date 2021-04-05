c39b7e0bc76db65c4c25d4885bebca00f349dfa9
Sean Cross <sean@xobs.io>
2021-04-05 10:03:08 +0800

    libstd: don't build `xous` as `restrict_std`
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/build.rs
+++ library/std/build.rs
@@ -36,7 +36,8 @@ fn main() {
         || target.contains("nintendo-3ds")
         || target.contains("vita")
         || target.contains("nto")
-    {
+        || target.contains("xous")
+        {
         // These platforms don't have any special requirements.
     } else {
         // This is for Cargo's build-std support, to mark std as unstable for
