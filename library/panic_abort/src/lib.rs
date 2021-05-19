8a35b39fc0ce4d7a304f448a9828db2bcdeac37d
Sean Cross <sean@xobs.io>
2021-05-19 20:53:24 +0800

    panic_abort: call __rustc_abort() for xous
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/panic_abort/src/lib.rs
+++ library/panic_abort/src/lib.rs
@@ -42,7 +42,7 @@ pub unsafe extern "C" fn __rust_start_panic(_payload: *mut &mut dyn BoxMeUp) -> 
             unsafe fn abort() -> ! {
                 libc::abort();
             }
-        } else if #[cfg(any(target_os = "hermit",
+        } else if #[cfg(any(target_os = "hermit", target_os = "xous",
                             all(target_vendor = "fortanix", target_env = "sgx")
         ))] {
             unsafe fn abort() -> ! {
