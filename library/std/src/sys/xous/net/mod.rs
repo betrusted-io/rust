c9d078c2873667f0e5bfcf9166ad12b0c1c6904a
bunnie <bunnie@kosagi.com>
2022-04-03 17:28:47 +0800

    move net-generic structures into mod

--- library/std/src/sys/xous/net/mod.rs
+++ library/std/src/sys/xous/net/mod.rs
@@ -19,6 +19,41 @@ macro_rules! unimpl {
     };
 }
 
+// this structure needs to be synchronized with what's in net/src/api.rs
+#[repr(C)]
+#[derive(Debug)]
+enum NetError {
+    // Ok = 0,
+    Unaddressable = 1,
+    SocketInUse = 2,
+    // AccessDenied = 3,
+    Invalid = 4,
+    // Finished = 5,
+    LibraryError = 6,
+    // AlreadyUsed = 7,
+    TimedOut = 8,
+}
+
+#[repr(C, align(4096))]
+struct ConnectRequest {
+    raw: [u8; 4096],
+}
+
+#[repr(C, align(4096))]
+struct SendData {
+    raw: [u8; 4096],
+}
+
+#[repr(C, align(4096))]
+pub struct ReceiveData {
+    raw: [u8; 4096],
+}
+
+#[repr(C, align(4096))]
+pub struct GetAddress {
+    raw: [u8; 4096],
+}
+
 pub struct TcpListener(!);
 
 impl TcpListener {
