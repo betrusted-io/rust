f9614797df2711ad824b47920aa025fcc51c0606
bunnie <bunnie@kosagi.com>
2022-04-04 22:38:53 +0800

    initial UDP implementation

--- library/std/src/sys/xous/net/udp.rs
+++ library/std/src/sys/xous/net/udp.rs
@@ -1,5 +1,4 @@
 use super::super::services;
-use crate::sys::unsupported;
 use crate::io;
 use crate::time::Duration;
 use crate::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
@@ -19,10 +18,11 @@ macro_rules! unimpl {
     };
 }
 
+#[derive(Clone)]
 pub struct UdpSocket {
     fd: usize,
     local: SocketAddr,
-    remote: Option<SocketAddr>,
+    remote: Cell<Option<SocketAddr>>,
     // in milliseconds. The setting applies only to `recv` calls after the timeout is set.
     read_timeout: Cell<u64>,
     // in milliseconds. The setting applies only to `send` calls after the timeout is set.
@@ -81,24 +81,26 @@ impl UdpSocket {
             let response = buf.as_slice::<u16>();
             if response[0] != 0 || valid.is_none() {
                 // errcode is a u8 but stuck in a u16 where the upper byte is invalid. Mask & decode accordingly.
-                let errcode = (response[4] & 0xff) as u8;
+                let errcode = (response[1] & 0xff) as u8;
                 if errcode == NetError::SocketInUse as u8 {
                     return Err(io::Error::new_const(io::ErrorKind::ResourceBusy, &"Socket in use"));
                 } else if errcode == NetError::Invalid as u8 {
                     return Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Port can't be 0 or invalid address"));
+                } else if errcode == NetError::LibraryError as u8 {
+                    return Err(io::Error::new_const(io::ErrorKind::Other, &"Library error"));
                 } else {
                     return Err(io::Error::new_const(io::ErrorKind::Other, &"Unable to connect or internal error"));
                 }
             }
             let fd = response[1] as usize;
             println!(
-                 "Connected with file handle of {}",
-                 fd
-            );
-            return Ok(UdpSocket {
+                "Connected with file handle of {}\r\n",
+                fd
+           );
+           return Ok(UdpSocket {
                 fd,
                 local: *addr,
-                remote: None,
+                remote: Cell::new(None),
                 read_timeout: Cell::new(0),
                 write_timeout: Cell::new(0),
                 handle_count: Arc::new(AtomicUsize::new(1)),
@@ -109,7 +111,7 @@ impl UdpSocket {
     }
 
     pub fn peer_addr(&self) -> io::Result<SocketAddr> {
-        match self.remote {
+        match self.remote.get() {
             Some(dest) => {
                 Ok(dest)
             }
@@ -123,7 +125,7 @@ impl UdpSocket {
         Ok(self.local)
     }
 
-    pub fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
+    fn recv_inner(&self, buf: &mut [u8], do_peek: bool) -> io::Result<(usize, SocketAddr)> {
         let mut receive_request = ReceiveData { raw: [0u8; 4096] };
 
         let range = unsafe {
@@ -139,16 +141,21 @@ impl UdpSocket {
                 *d = s;
             }
         }
+        let peek = if do_peek {
+            Some(core::num::NonZeroUsize::new(1).unwrap())
+        } else {
+            None
+        };
         if let Ok(xous::Result::MemoryReturned(_offset, _valid)) = xous::send_message(
             services::network(),
             xous::Message::new_lend_mut(
                 42 | (self.fd << 16), /* StdUdpRx */
                 range,
-                None,
+                peek,
                 None,
             ),
         ) {
-            if receive_request.raw[0] != 0 {
+            if receive_request.raw[0] != 0 { // error case
                 if receive_request.raw[1] == NetError::TimedOut as u8 {
                     return Err(io::Error::new_const(
                         io::ErrorKind::TimedOut,
@@ -159,6 +166,8 @@ impl UdpSocket {
                         io::ErrorKind::WouldBlock,
                         &"recv would block",
                     ));
+                } else if receive_request.raw[1] == NetError::LibraryError as u8 {
+                    return Err(io::Error::new_const(io::ErrorKind::Other, &"Library error"));
                 } else {
                     return Err(io::Error::new_const(
                         io::ErrorKind::Other,
@@ -207,20 +216,134 @@ impl UdpSocket {
         }
     }
 
+    pub fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
+        self.recv_inner(buf, false)
+    }
+
     pub fn recv(&self, buf: &mut [u8]) -> io::Result<usize> {
         self.recv_from(buf).map(|(len, _addr)| len)
     }
 
-    pub fn peek_from(&self, _: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        unimpl!();
+    pub fn peek_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
+        self.recv_inner(buf, true)
     }
 
-    pub fn send_to(&self, _: &[u8], _: &SocketAddr) -> io::Result<usize> {
-        unimpl!();
+    pub fn peek(&self, buf: &mut [u8]) -> io::Result<usize> {
+        self.peek_from(buf).map(|(len, _addr)| len)
+    }
+
+    pub fn connect(&self, maybe_addr: io::Result<&SocketAddr>) -> io::Result<()> {
+        let addr = maybe_addr?;
+        self.remote.set(Some(*addr));
+        Ok(())
+    }
+
+    pub fn send(&self, buf: &[u8]) -> io::Result<usize> {
+        if let Some(addr) = self.remote.get() {
+            self.send_to(buf, &addr)
+        } else {
+            Err(io::Error::new_const(io::ErrorKind::NotConnected, &"No remote specified"))
+        }
+    }
+
+    pub fn send_to(&self, buf: &[u8], addr: &SocketAddr) -> io::Result<usize> {
+        let mut tx_req = SendData { raw: [0u8; 4096] };
+
+        // Construct the request.
+        let port_bytes = addr.port().to_le_bytes();
+        tx_req.raw[0] = port_bytes[0];
+        tx_req.raw[1] = port_bytes[1];
+        match addr.ip() {
+            IpAddr::V4(addr) => {
+                tx_req.raw[2] = 4;
+                for (dest, src) in tx_req.raw[3..].iter_mut().zip(addr.octets()) {
+                    *dest = src;
+                }
+            }
+            IpAddr::V6(addr) => {
+                tx_req.raw[2] = 6;
+                for (dest, src) in tx_req.raw[3..].iter_mut().zip(addr.octets()) {
+                    *dest = src;
+                }
+            }
+        }
+        let len = buf.len() as u16;
+        let len_bytes = len.to_le_bytes();
+        tx_req.raw[19] = len_bytes[0];
+        tx_req.raw[20] = len_bytes[1];
+        for (&s, d) in buf.iter().zip(tx_req.raw[21..].iter_mut()) {
+            *d = s;
+        }
+
+        let buf = unsafe {
+            xous::MemoryRange::new(
+                &mut tx_req as *mut SendData as usize,
+                core::mem::size_of::<SendData>(),
+            )
+            .unwrap()
+        };
+
+        // write time-outs are implemented on the caller side. Basically, if the Net crate server
+        // is too busy to take the call immediately: retry, until the timeout is reached.
+        let now = crate::time::Instant::now();
+        let write_timeout = if self.nonblocking.get() {
+            // nonblocking
+            core::time::Duration::ZERO
+        } else {
+            // blocking
+            if self.write_timeout.get() == 0 { // forever
+                core::time::Duration::from_millis(u64::MAX)
+            } else { // or this amount of time
+                core::time::Duration::from_millis(self.write_timeout.get())
+            }
+        };
+        loop {
+            let response = xous::try_send_message(
+                services::network(),
+                xous::Message::new_lend_mut(
+                    43, /* StdUdpTx */
+                    buf,
+                    None,
+                    xous::MemorySize::new(4096),
+                ),
+            );
+            match response {
+                Ok(xous::Result::MemoryReturned(_, valid)) => {
+                    // The first four bytes should be zero upon success, and will be nonzero
+                    // for an error.
+                    let response = buf.as_slice::<u16>();
+                    if response[0] != 0 || valid.is_none() {
+                        let errcode = (response[1] & 0xff) as u8;
+                        if errcode == NetError::SocketInUse as u8 {
+                            return Err(io::Error::new_const(io::ErrorKind::ResourceBusy, &"Socket in use"));
+                        } else if errcode == NetError::Invalid as u8 {
+                            return Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Socket not valid"));
+                        } else if errcode == NetError::LibraryError as u8 {
+                            return Err(io::Error::new_const(io::ErrorKind::Other, &"Library error"));
+                        } else {
+                            return Err(io::Error::new_const(io::ErrorKind::Other, &"Unable to connect"));
+                        }
+                    } else {
+                        // no error
+                        return Ok(len as usize);
+                    }
+                }
+                Ok(xous::Result::RetryCall) | Err(xous::Error::ServerQueueFull) => {
+                    if now.elapsed() >= write_timeout {
+                        return Err(io::Error::new_const(io::ErrorKind::WouldBlock, &"Write timed out"));
+                    } else {
+                        // question: do we want to do something a bit more gentle than immediately retrying?
+                        xous::yield_slice();
+                    }
+                }
+                _ => return Err(io::Error::new_const(io::ErrorKind::Other, &"Library error")),
+            }
+        }
     }
 
     pub fn duplicate(&self) -> io::Result<UdpSocket> {
-        unimpl!();
+        self.handle_count.fetch_add(1, Ordering::Relaxed);
+        Ok(self.clone())
     }
 
     pub fn set_read_timeout(&self, timeout: Option<Duration>) -> io::Result<()> {
@@ -259,6 +382,56 @@ impl UdpSocket {
         }
     }
 
+    pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
+        if ttl > 255 {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "TTL must be less than 256"));
+        }
+        xous::send_message(
+            services::network(),
+            xous::Message::new_blocking_scalar(
+                37 | ((self.fd as usize) << 16), //StdSetTtl = 37
+                ttl as usize,
+                0,
+                0,
+                1, // specify udp
+            ),
+        )
+        .or(Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Unexpected return value")))
+        .map(|_| ())
+    }
+
+    pub fn ttl(&self) -> io::Result<u32> {
+        xous::send_message(
+            services::network(),
+            xous::Message::new_blocking_scalar(
+                36 | ((self.fd as usize) << 16), //StdGetTtl = 36
+                0,
+                0,
+                0,
+                1, // specify udp
+            ),
+        )
+        .or(Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Unexpected return value")))
+        .and_then(|res| {
+            if let xous::Result::Scalar1(ttl) = res {
+                Ok(ttl as u32)
+            } else {
+                Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Unexpected return value"))
+            }
+        })
+    }
+
+    pub fn take_error(&self) -> io::Result<Option<io::Error>> {
+        // this call doesn't have a meaning on our platform, but we can at least not panic if it's used.
+        Ok(None)
+    }
+
+    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
+        self.nonblocking.set(nonblocking);
+        Ok(())
+    }
+
+    // ------------- smoltcp base stack does not have multicast or broadcast support ---------------
     pub fn set_broadcast(&self, _: bool) -> io::Result<()> {
         unimpl!();
     }
@@ -306,46 +479,6 @@ impl UdpSocket {
     pub fn leave_multicast_v6(&self, _: &Ipv6Addr, _: u32) -> io::Result<()> {
         unimpl!();
     }
-
-    pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
-        xous::send_message(
-            self.fd as _,
-            xous::Message::new_blocking_scalar(
-                37 | ((self.fd as usize) << 16), //StdSetTtl = 37
-                ttl as usize,
-                0,
-                0,
-                0,
-            ),
-        )
-        .or(Err(io::Error::new_const(io::ErrorKind::InvalidInput, &"Unexpected return value")))
-        .map(|_| ())
-    }
-
-    pub fn ttl(&self) -> io::Result<u32> {
-        unimpl!();
-    }
-
-    pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        unimpl!();
-    }
-
-    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
-        self.nonblocking.set(nonblocking);
-        Ok(())
-    }
-
-    pub fn peek(&self, _: &mut [u8]) -> io::Result<usize> {
-        unimpl!();
-    }
-
-    pub fn send(&self, _: &[u8]) -> io::Result<usize> {
-        unimpl!();
-    }
-
-    pub fn connect(&self, _: io::Result<&SocketAddr>) -> io::Result<()> {
-        unimpl!();
-    }
 }
 
 impl fmt::Debug for UdpSocket {
@@ -353,7 +486,28 @@ impl fmt::Debug for UdpSocket {
         write!(
             f,
             "UDP listening on {:?} to {:?}",
-            self.local, self.remote,
+            self.local, self.remote.get(),
         )
     }
+}
+
+impl Drop for UdpSocket {
+    fn drop(&mut self) {
+        if self.handle_count.fetch_sub(1, Ordering::Relaxed) == 1 { // only drop if we're the last clone
+            match xous::send_message(
+                services::network(),
+                xous::Message::new_blocking_scalar(41 | ((self.fd as usize) << 16), // StdUdpClose
+                0, 0, 0, 0)
+            ) {
+                Ok(xous::Result::Scalar1(result)) => {
+                    if result != 0 {
+                        println!("UdpSocket drop failure err code {}\r\n", result);
+                    }
+                },
+                _ => {
+                    println!("UdpSocket drop failure - internal error\r\n");
+                }
+            }
+        }
+   }
 }
