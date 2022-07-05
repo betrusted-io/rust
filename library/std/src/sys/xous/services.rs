bbf2d9f9ac77fe40c36bf4c33800583c4399a468
Sean Cross <sean@xobs.io>
2022-07-05 22:59:06 +0800

    xous: services: harmonise names and add pddb
    Add pddb as a service, and remove the redundant naming used in this
    file.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/sys/xous/services.rs
+++ library/std/src/sys/xous/services.rs
@@ -2,51 +2,63 @@ use core::sync::atomic::{AtomicU32, Ordering};
 use xous::services::nameserver as ns;
 
 pub(crate) fn network() -> xous::CID {
-    static NETWORK_CID: AtomicU32 = AtomicU32::new(0);
-    let cid = NETWORK_CID.load(Ordering::Relaxed);
+    static CID: AtomicU32 = AtomicU32::new(0);
+    let cid = CID.load(Ordering::Relaxed);
     if cid != 0 {
         return cid;
     }
 
     let cid = ns::connect("_Middleware Network Server_").unwrap();
-    NETWORK_CID.store(cid, Ordering::Relaxed);
+    CID.store(cid, Ordering::Relaxed);
     cid
 }
 
 pub(crate) fn dns() -> xous::CID {
-    static DNS_CID: AtomicU32 = AtomicU32::new(0);
-    let cid = DNS_CID.load(Ordering::Relaxed);
+    static CID: AtomicU32 = AtomicU32::new(0);
+    let cid = CID.load(Ordering::Relaxed);
     if cid != 0 {
         return cid;
     }
 
     let cid = ns::connect("_DNS Resolver Middleware_").unwrap();
-    DNS_CID.store(cid, Ordering::Relaxed);
+    CID.store(cid, Ordering::Relaxed);
+    cid
+}
+
+pub(crate) fn pddb() -> xous::CID {
+    static CID: AtomicU32 = AtomicU32::new(0);
+    let cid = CID.load(Ordering::Relaxed);
+    if cid != 0 {
+        return cid;
+    }
+
+    let cid = ns::connect("_Plausibly Deniable Database_").unwrap();
+    CID.store(cid, Ordering::Relaxed);
     cid
 }
 
 pub(crate) fn ticktimer() -> xous::CID {
     // Sleep is done by connecting to the ticktimer server and sending
     // a blocking message.
-    static TICKTIMER_CID: AtomicU32 = AtomicU32::new(0);
-    let cid = TICKTIMER_CID.load(Ordering::Relaxed);
+    static CID: AtomicU32 = AtomicU32::new(0);
+    let cid = CID.load(Ordering::Relaxed);
     if cid != 0 {
         return cid;
     }
 
     let cid = xous::connect(xous::SID::from_bytes(b"ticktimer-server").unwrap()).unwrap();
-    TICKTIMER_CID.store(cid, Ordering::Relaxed);
+    CID.store(cid, Ordering::Relaxed);
     cid
 }
 
 pub(crate) fn systime() -> xous::CID {
-    static SYSTIME_CID: AtomicU32 = AtomicU32::new(0);
-    let cid = SYSTIME_CID.load(Ordering::Relaxed);
+    static CID: AtomicU32 = AtomicU32::new(0);
+    let cid = CID.load(Ordering::Relaxed);
     if cid != 0 {
         return cid;
     }
 
     let cid = xous::connect(xous::SID::from_bytes(b"timeserverpublic").unwrap()).unwrap();
-    SYSTIME_CID.store(cid, Ordering::Relaxed);
+    CID.store(cid, Ordering::Relaxed);
     cid
 }
