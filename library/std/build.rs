1b8ee043d7bf8f1ad56060ad694c710c0ec86a5d
Sean Cross <sean@xobs.io>
2022-09-24 09:41:09 +0800

    std: run `cargo fmt`
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/build.rs
+++ library/std/build.rs
@@ -32,7 +32,7 @@ fn main() {
         || target.contains("solid")
         || target.contains("nintendo-3ds")
         || target.contains("xous")
-        {
+    {
         // These platforms don't have any special requirements.
     } else {
         // This is for Cargo's build-std support, to mark std as unstable for
