d61cab13695ba06eb16dc3dc6e7760147dcfe319
Sean Cross <sean@xobs.io>
2022-10-31 11:29:52 +0800

    ffi: add `std::os::services::connect()`
    Add a libstd call to connect to a particular service by name. As part of
    this, ffi functions are exposed in `std::os::xous::ffi`.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/os/xous/mod.rs
+++ library/std/src/os/xous/mod.rs
@@ -5,6 +5,9 @@ pub mod ffi;
 pub mod fs;
 pub mod path;
 
+#[stable(feature = "rust1", since = "1.0.0")]
+pub mod services;
+
 /// A prelude for conveniently writing platform-specific code.
 ///
 /// Includes all extension traits, and some important type definitions.
