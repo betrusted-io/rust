79c9169c397abf5aff8d0ec9b29de61554d7dfea
Sean Cross <sean@xobs.io>
2022-11-04 11:05:13 +0800

    xous: forward-port to rust-1.65.0
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/sys/xous/common.rs
+++ library/std/src/sys/xous/common.rs
@@ -14,7 +14,7 @@ pub fn _start() {
 }
 
 #[cfg(not(test))]
-pub unsafe fn init(_argc: isize, _argv: *const *const u8) {}
+pub unsafe fn init(_argc: isize, _argv: *const *const u8, _sigpipe: u8) {}
 
 // SAFETY: must be called only once during runtime cleanup.
 // NOTE: this is not guaranteed to run, for example when the program aborts.
