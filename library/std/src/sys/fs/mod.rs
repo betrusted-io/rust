6a17cd034cf141c2d1ae86ec98ffa9eb5e8351a4
Sean Cross <sean@osdyne.com>
2024-08-06 22:45:11 +0800

    xous: fs: add initial filesystem support
    This also adds senres, which is a method for sending and receiving structs.
    It is a cut-down version of rkyv and similar modules.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/sys/fs/mod.rs
+++ library/std/src/sys/fs/mod.rs
@@ -45,6 +45,10 @@ cfg_select! {
         mod vexos;
         use vexos as imp;
     }
+    target_os = "xous" => {
+        mod xous;
+        use xous as imp;
+    }
     _ => {
         mod unsupported;
         use unsupported as imp;
